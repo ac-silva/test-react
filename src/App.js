8325cea5387bb3c3426b99b155d4b1c82a9ffea1
Arthur Carvalho <arthur.carvalho@e-thinkers.com.br>
2020-02-11 08:33:55 -0300

    Configured routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserList from './pages/UserList';
+import UserView from './pages/UserView';
 
 function App() {
   return (
     <div className="app">
-      <UserList></UserList>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/user-list">
+            <UserList />
+          </Route>
+          <Route path={`/user-view/:userId`}>
+            <UserView />
+          </Route>
+          <Route path="/user-view">
+            <UserView />
+          </Route>
+          <Route path="/">
+            <UserList />
+          </Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
