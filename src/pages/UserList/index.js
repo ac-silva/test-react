3b52d61ed1d18495f72b263d3ac6672efdddbc45
Arthur Carvalho <arthur.carvalho@e-thinkers.com.br>
2020-02-11 08:38:36 -0300

    Refactored to use api handler and fix layout of userlist page

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '../../components/Table';
+import UserHandler from '../../api/UserHandler';
 import './userlist.scss';
+import {Link} from 'react-router-dom';
 
 export default function UserList(props) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    UserHandler().getUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
+ 
   return (
-    <Table className="table-bordered">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Nome</th>
-          <th>Sobrenome</th>
-          <th>Email</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>
-            <button>View</button>
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+    <div className="container">
+      <Table className="table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Email</th>
+            <th className="align-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td className="align-center">
+                  <Link to={`/user-view/${user.id}`}><i className="fa fa-eye"></i></Link>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    </div>
   )
 }
