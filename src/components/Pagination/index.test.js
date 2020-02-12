18cf397189931458df703ceb286305fecb1ce0a2
Arthur Carvalho <arthur.carvalho@e-thinkers.com.br>
2020-02-11 23:06:52 -0300

    Fix travis.yml configuration and fix test(s) specs

--- src/components/Pagination/index.test.js
+++ src/components/Pagination/index.test.js
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react';
 describe('comportamento', () => {
   it('renderiza marcador corretamentem na página selecionada', () => {
     const {container} = render(
-      <Pagination max={2} currentPage={1}></Pagination>
+      <Pagination max={2} currentPage={1} onChange={(page) => {}}></Pagination>
     );
     const marker = container.querySelector('a.active');
     expect(marker.innerText).toEqual(1);
