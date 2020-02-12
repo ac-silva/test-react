0a3a438d83837bb55951b25d3c9feb45ee96cee4
Arthur Carvalho <arthur.carvalho@e-thinkers.com.br>
2020-02-11 23:00:58 -0300

    Fix error when creating component without onChange event

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import './pagination.scss';
 
 export default function Pagination(props) {
+  const onChange    = (props.onChange) ? props.onChange : (index) => { throw 'pagination onChange event not implemented!' }; 
+  const currentPage = (props.currentPage) ? props.currentPage : -1;
 
   if(!props.max) return '';
-
   return (
     <div className="pagination">
-      <a href="#" onClick={props.onChange(1)} className="first">&laquo;</a>
+      <a href="#" onClick={onChange(1)} className="first">&laquo;</a>
       { 
         [...Array(props.max).keys()].map((index) => {
+          const pageIndex = index + 1;
           return (
             <a key={index} href="#" 
-              onClick={props.onChange(index+1)} 
-              className={(props.currentPage === (index+1)) ? 'active' : ''}>
-              {index + 1}
+              onClick={onChange(pageIndex)} 
+              className={(currentPage === pageIndex) ? 'active' : ''}>
+              {pageIndex}
             </a>
           )
         })
       }
-      <a href="#" onClick={props.onChange(props.max)} className="last">&raquo;</a>
+      <a href="#" onClick={onChange(props.max)} className="last">&raquo;</a>
     </div>
   )
   
